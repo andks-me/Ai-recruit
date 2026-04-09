ff90956849b8649ffc7756f39d2c0336c01c31fe
andks-me <andreeva.ks.an@gmail.com>
2026-04-09 08:41:16 +0300

    Enhance agent learning and skills system with new features and optimizations.

---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule RecruitmentSystem.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :recruitment_system,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {RecruitmentSystem.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:httpoison, "~> 1.8"},
+      {:jason, "~> 1.4"},
+      {:quantum, "~> 3.4"},
+      {:crontab, "~> 1.1"},
+      {:phoenix, "~> 1.7"},
+      {:phoenix_live_view, "~> 0.19"},
+      {:plug_cowboy, "~> 2.6"},
+      {:nadia, "~> 0.7"},
+      {:floki, "~> 0.34"},
+      {:oban, "~> 2.13"}
+    ]
+  end
+end
